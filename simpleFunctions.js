fbaa9a994aff3c87920f5978c9908eea1bd4c4ae
Roman <roman.ilienko@corp.kismia.com>
2022-02-10 14:25:05 +0200

    uploaded

---
+++ simpleFunctions.js
@@ -0,0 +1,26 @@
+const getUsers = () => {
+  return [
+    { id: 1, name: 'Bob' },
+    { id: 2, name: 'Steve' },
+    { id: 3, name: 'Kelly' },
+  ];
+};
+
+const getActivities = () => {
+  return [
+    { id: 1, task: 'do something', userId: 1 },
+    { id: 2, task: 'do something', userId: 1 },
+    { id: 3, task: 'do something', userId: 1 },
+    { id: 4, task: 'do something', userId: 2 },
+    { id: 5, task: 'do something', userId: 2 },
+    { id: 6, task: 'do something', userId: 2 },
+  ];
+};
+
+// Your code bellow
+
+const getUser = name => {};
+
+const getActivitiesForUser = name => {};
+
+const getUserWithActivities = name => {};
